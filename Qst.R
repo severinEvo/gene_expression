e1ecc9b2c566ae00bcf78c89adce5072f2ff9db7
severinEvo <SUebbing@gmx.de>
2015-07-10 11:49:53 +0200

    Added general way of dealing with missing factor levels.

--- Qst.R
+++ Qst.R
@@ -7,24 +7,18 @@ Qst<-function(i, spec, tis=NULL, sex=NULL){
   lsp2<-length(spec[levels(spec)==levels(spec)[2]])
   n0<-length(i)-(lsp1^2+lsp2^2)/length(i)
 
-  if(length(levels(sex))==0 | is.null(sex)){
-    dataset<-data.frame(expr=i,spec=spec)
-    anova_0<-anova(aov(dataset$expr ~ dataset$spec))
-  }else if((length(levels(sex))==0 | is.null(sex)) & length(levels(tis))>1){
-    dataset<-data.frame(expr=i,spec=spec,tis=tis)
-    anova_0<-anova(aov(dataset[,1] ~ dataset$spec * dataset$tis))
-  }else if(length(levels(sex))==2 & (length(levels(tis))==0 | is.null(tis))){
-    dataset<-data.frame(expr=i,spec=spec,sex=sex)
-    anova_0<-anova(aov(dataset[,1] ~ dataset$spec * dataset$sex))
-  }else if(length(levels(sex))==2 & length(levels(tis))>1){
-    dataset<-data.frame(expr=i,spec=spec,tis=tis,sex=sex)
-    if(sum(!is.na(dataset[,1]))<20 & sum(!is.na(dataset[,1]))>10){
-      anova_0<-anova(aov(dataset[,1] ~ dataset$spec * dataset$sex))
-    }else if(sum(!is.na(dataset[,1]))<11){
-    anova_0<-anova(aov(dataset$expr ~ dataset$spec))
-    }else{
-      anova_0<-anova(aov(dataset[,1] ~ dataset$spec * dataset$sex * dataset$tis))
-  }}
+  dataset<-data.frame(expr=i,spec=spec,tis=tis,sex=sex)
+  dataset_0<-dataset[!is.na(dataset[,1]),]
+  if(length(levels(droplevels(dataset_0$tis)))<2 & length(levels(droplevels(dataset_0$tis)))==1){
+    anova_0<-anova(aov(dataset_0[,1] ~ dataset_0$spec))
+  }else if(length(levels(droplevels(dataset_0$tis)))>1 & length(levels(droplevels(dataset_0$sex)))==1){
+    anova_0<-anova(aov(dataset_0[,1] ~ dataset_0$spec * dataset_0$tis))
+  }else if(length(levels(droplevels(dataset_0$tis)))<2 & length(levels(droplevels(dataset_0$sex)))==2){
+    anova_0<-anova(aov(dataset_0[,1] ~ dataset_0$spec * dataset_0$sex))
+  }else if(length(levels(droplevels(dataset_0$tis)))>1 & length(levels(droplevels(dataset_0$sex)))==2){
+    anova_0<-anova(aov(dataset_0[,1] ~ dataset_0$spec * dataset_0$tis * dataset_0$sex))
+  }else stop('Unknown error.')
+
   vW<-tail(anova_0$'Mean Sq',n=1)
   vB<-(anova_0[1,3]-vW)/n0
   qst<-vB/(vB+2*vW)
