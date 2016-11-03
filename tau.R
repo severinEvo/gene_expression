0871ef1738bf18a80a0551984f7962afc3900094
severinEvo <SUebbing@gmx.de>
2016-11-03 11:16:44 -0400

    Performance update.

--- tau.R
+++ tau.R
@@ -1,7 +1,5 @@
 tau<-function(x){
   if(any(is.na(x))) stop('NA\'s need to be 0.')
   if(any(x<0)) stop('Negative input values not permitted. Maybe data is log transformed?')
-  d<-0
-  for(i in 1:length(x)) d<-d+(1-x[i]/max(x))
-  t<-d/(length(x)-1)
+  t<-sum(1-x/max(x))/(length(x)-1)
 }
